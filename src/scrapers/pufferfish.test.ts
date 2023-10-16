55ecefc75905edcc4c4eaa8c4f831744f6ce72f5
Petr Václavek <petr@vaclavek.cloud>
2023-10-16 20:45:57 +0200

    test refactor

--- src/scrapers/pufferfish.test.ts
+++ src/scrapers/pufferfish.test.ts
@@ -4,6 +4,7 @@ import { PufferfishScraper } from "./pufferfish";
 describe("Pufferfish Scraper", () => {
     test("Get download url for jar version", async () => {
         const mockPufferfishDownloadUrl = "https://ci.pufferfish.host/job/Pufferfish-1.19/73/artifact/build/libs/pufferfish-paperclip-1.19.4-R0.1-SNAPSHOT-reobf.jar"
-        expect(await PufferfishScraper("1.19")).toBe(mockPufferfishDownloadUrl)
+        const downloadUrl = await PufferfishScraper("1.19")
+        expect(downloadUrl).toBe(mockPufferfishDownloadUrl)
     }, {retry: 3, timeout: 600})
 })
