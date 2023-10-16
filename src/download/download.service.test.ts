39e49404210accb578dc05db500b8a86a2c6fa6f
Petr Václavek <petr@vaclavek.cloud>
2023-10-16 20:46:18 +0200

    fix test refactor

--- src/download/download.service.test.ts
+++ src/download/download.service.test.ts
@@ -24,7 +24,7 @@ describe("Download service", () => {
         expect(await DownloadService().generateUrlModrinth(mockModrinthUrl)).toBe(mocktModrinthDownloadUrl)
     })
 
-    test("If it we download from url", async () => {
+    test("If it we can download server jar", async () => {
         const tmpFilePath = "/tmp/server.jar"
         if(await Bun.file(tmpFilePath).exists()) unlinkSync(tmpFilePath)
         const downloadUrl = await DownloadService().generateUrlServerJar("paper", "1.20.2")
