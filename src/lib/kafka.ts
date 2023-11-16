b0cfc5a6fde9f1bd66c6ddb3c23162add8becced
Petr Václavek <petr@vaclavek.cloud>
2023-11-16 09:44:12 +0100

    feat: kafka development mode

--- src/lib/kafka.ts
+++ src/lib/kafka.ts
@@ -1,13 +1,16 @@
 import { Kafka } from "kafkajs";
 
-const { KAFKA_HOST, KAFKA_USERNAME, KAFKA_PASSWORD } = process.env
+const { NODE_ENV, KAFKA_HOST, KAFKA_USERNAME, KAFKA_PASSWORD } = process.env
 
-export const kafkaClient = new Kafka({
-  brokers: [KAFKA_HOST ?? ""],
-  sasl: {
-    mechanism: "scram-sha-256",
-    username: KAFKA_USERNAME ?? "",
-    password: KAFKA_PASSWORD ?? "",
-  },
-  ssl: true
-})
+
+export const kafkaClient = NODE_ENV == "production" ?
+  new Kafka({
+    brokers: [KAFKA_HOST ?? ""],
+    sasl: {
+      mechanism: "scram-sha-256",
+      username: KAFKA_USERNAME ?? "",
+      password: KAFKA_PASSWORD ?? "",
+    },
+    ssl: true
+  }) :
+  new Kafka({ brokers: [KAFKA_HOST ?? ""] })
